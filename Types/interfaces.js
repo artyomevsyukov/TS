344f568b0465541585c99b1aaa7c6bae84ea5121
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-07-08 10:16:12 +0300

    типизация ответа через interface

--- Types/interfaces.js
+++ Types/interfaces.js
@@ -1,14 +1,38 @@
 "use strict";
+// interface User {
+//   name: string
+//   age: number
+//   skills: string[]
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-const user = {
-    name: "Alex",
-    age: 22,
-    skills: ["1"],
-};
-const user2 = {
-    name: "Alex",
-    age: 22,
-    skills: ["1"],
-    id: 3,
-    createdAt: new Date(),
-};
+var PaymentStatus;
+(function (PaymentStatus) {
+    PaymentStatus["Success"] = "success";
+    PaymentStatus["Failed"] = "failed";
+})(PaymentStatus || (PaymentStatus = {}));
+function sendPaymentRequest({ sum, from, to, }) {
+    const isSuccess = Math.random() > 0.5;
+    if (isSuccess) {
+        const successResponse = {
+            status: PaymentStatus.Success,
+            data: {
+                sum,
+                from,
+                to,
+                datasetId: Date.now(),
+            },
+        };
+        return successResponse;
+    }
+    else {
+        const failedResponse = {
+            status: PaymentStatus.Failed,
+            data: {
+                errorMessage: "Недостаточно средств",
+                errorCode: 403,
+            },
+        };
+        return failedResponse;
+    }
+}
+console.log(sendPaymentRequest({ sum: 1000, from: 112312314, to: 5353465346 }));
--- Types/interfaces.ts
+++ Types/interfaces.ts
@@ -1,33 +1,91 @@
-interface User {
-  name: string
-  age: number
-  skills: string[]
+// interface User {
+//   name: string
+//   age: number
+//   skills: string[]
+// }
+
+// const user: User = {
+//   name: "Alex",
+//   age: 22,
+//   skills: ["1"],
+// }
+
+// interface Role {
+//   id: number
+// }
+
+// interface UserWithRole extends User, Role {
+//   createdAt: Date
+// }
+
+// const user2: UserWithRole = {
+//   name: "Alex",
+//   age: 22,
+//   skills: ["1"],
+//   id: 3,
+//   createdAt: new Date(),
+// }
+
+// interface UserDic {
+//   [index: number]: User
+// }
+
+interface IPayment {
+  sum: number
+  from: number
+  to: number
 }
 
-const user: User = {
-  name: "Alex",
-  age: 22,
-  skills: ["1"],
+interface IPaymentRequest extends IPayment {}
+
+interface IPaymentResponse extends IPayment {
+  datasetId: number
 }
 
-interface Role {
-  id: number
+interface IPaymentSuccess {
+  status: PaymentStatus.Success
+  data: IPaymentResponse
+}
+interface IPaymentFailed {
+  status: PaymentStatus.Failed
+  data: { errorMessage: string; errorCode: number }
 }
 
-interface UserWithRole extends User, Role {
-  createdAt: Date
+enum PaymentStatus {
+  Success = "success",
+  Failed = "failed",
 }
 
-const user2: UserWithRole = {
-  name: "Alex",
-  age: 22,
-  skills: ["1"],
-  id: 3,
-  createdAt: new Date(),
+function sendPaymentRequest({
+  sum,
+  from,
+  to,
+}: IPaymentRequest): IPaymentSuccess | IPaymentFailed {
+  const isSuccess = Math.random() > 0.5
+
+  if (isSuccess) {
+    const successResponse: IPaymentSuccess = {
+      status: PaymentStatus.Success,
+      data: {
+        sum,
+        from,
+        to,
+        datasetId: Date.now(),
+      },
+    }
+    return successResponse
+  } else {
+    const failedResponse: IPaymentFailed = {
+      status: PaymentStatus.Failed,
+      data: {
+        errorMessage: "Недостаточно средств",
+        errorCode: 403,
+      },
+    }
+    return failedResponse
+  }
 }
 
-interface UserDic {
-  [index: number]: User
-}
+console.log(sendPaymentRequest({ sum: 1000, from: 112312314, to: 5353465346 }))
 
 export {}
