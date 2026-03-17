8f1e5f772af0c777474b9dba8a10345eda1cc72c
Artyom Evsyukov <artyom.evsyukov@gmail.com>
2026-03-17 14:10:14 +0300

    43-Ivan

---
+++ Ivan/41index.ts
@@ -0,0 +1,54 @@
+// Requests
+
+type Animal = 'cat' | 'dog' | 'bird';
+
+enum AnimalStatus {
+  Available = 'available',
+  NotAvailable = 'not available',
+}
+
+// Response #1
+interface AnimalData {
+  animal: Animal;
+  breed: string;
+  sterilized?: string;
+}
+
+interface AnimalAvailableData extends AnimalData {
+  location: string;
+  age?: number;
+}
+
+// Response #2
+interface AnimalNotAvailableData {
+  message: string;
+  nextUpdateIn: Date;
+}
+
+interface AnimalAvailableResponse {
+  status: AnimalStatus.Available;
+  data: AnimalAvailableData;
+}
+
+interface AnimalNotAvailableResponse {
+  status: AnimalStatus.NotAvailable;
+  data: AnimalNotAvailableData;
+}
+
+type Response = AnimalAvailableResponse | AnimalNotAvailableResponse;
+
+function isAvailable(response: Response): response is AnimalAvailableResponse {
+  if (response.status === AnimalStatus.Available) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkAnimalData(animal: Response): AnimalAvailableData | string {
+  if (isAvailable(animal)) {
+    return animal.data;
+  } else {
+    return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
+  }
+}
